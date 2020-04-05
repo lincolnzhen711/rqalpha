b4d3e1d907248ea8a4b03068b6f8102e0f2cdfa6
tensionlee <tensionlee@aliyun.com>
2020-04-05 17:23:52 +0800

    test

--- rqalpha/examples/buy_and_hold.py
+++ rqalpha/examples/buy_and_hold.py
@@ -1,4 +1,5 @@
 from rqalpha.api import *
+from rqalpha import run_func
 
 
 # 在这个方法中编写任何的初始化逻辑。context对象将会在你的算法策略的任何方法之间做传递。
@@ -24,7 +25,37 @@ def handle_bar(context, bar_dict):
     # 使用order_shares(id_or_ins, amount)方法进行落单
 
     # TODO: 开始编写你的算法吧！
+
     if not context.fired:
         # order_percent并且传入1代表买入该股票并且使其占有投资组合的100%
         order_percent(context.s1, 1)
+        logger.info(context.s1)
         context.fired = True
+
+
+config = {
+  "base": {
+    "start_date": "2016-06-01",
+    "end_date": "2016-12-01",
+    "benchmark": "000300.XSHG",
+    "accounts": {
+        "stock": 1000000
+    }
+  },
+  "extra": {
+    "log_level": "verbose",
+  },
+  "mod": {
+    "sys_analyser": {
+      "enabled": True,
+      "plot": True
+    }
+  }
+}
+
+
+# 您可以指定您要传递的参数
+run_func(init=init, before_trading=before_trading, handle_bar=handle_bar, config=config)
+
+# 如果你的函数命名是按照 API 规范来，则可以直接按照以下方式来运行
+# run_func(**globals())
